6d352e443c6a04b8d6297842e653d01ef3d48ab2
aleem javeed <aleemjaveed@hotmail.com>
2018-02-07 23:13:49 +0000

    created fleet app and user interface

--- fleetApp/fleetApp/ViewController.swift
+++ fleetApp/fleetApp/ViewController.swift
@@ -1,31 +1,24 @@
 //
 //  ViewController.swift
-//  FoodTrcker
+//  fleetApp
 //
-//  Created by Aleem Javeed on 03/02/2018.
+//  Created by Aleem Javeed on 07/02/2018.
 //  Copyright © 2018 Aleem Javeed. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-    // Mark: Properties
-    @IBOutlet weak var nameTextField: UITextField!
-    
-    @IBOutlet weak var mealNameLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-    //Mark: Actions
-    @IBAction func setDefaultLabelText(_ sender: Any) {
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
-    
-    
-
-    
-
-   
 
 
 }
