886c354ffb1a74392f5ed1967a723e7179d0e1bb
aleem javeed <aleemjaveed@hotmail.com>
2018-03-08 00:05:22 +0000

    side menu complete

---
+++ FleetV3/Fleetv3/Fleetv3/CustomerMenuTVC.swift
@@ -0,0 +1,19 @@
+//
+//  CustomerMenuTVC.swift
+//  Fleetv3
+//
+//  Copyright © 2018 Aleem Javeed. All rights reserved.
+//
+
+import UIKit
+
+class CustomerMenuTVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        
+        view.backgroundColor = UIColor(red: 0.231, green: 0.353, blue: 0.498, alpha: 1.0)
+    }
+
+}
