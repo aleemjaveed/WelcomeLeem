abf5d39a3715c2e80361807e67f7d3aecc7643b7
aleem javeed <aleemjaveed@hotmail.com>
2018-03-08 02:16:52 +0000

    product ui created

---
+++ FleetV3/Fleetv3/Fleetv3/ProductListTVC.swift
@@ -0,0 +1,31 @@
+//
+//  ProductListTVC.swift
+//  Fleetv3
+//
+//  Copyright © 2018 Aleem Javeed. All rights reserved.
+//
+
+import UIKit
+
+class ProductListTVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProductCell", for: indexPath)
+        
+        return cell
+    }
+
+
+}
