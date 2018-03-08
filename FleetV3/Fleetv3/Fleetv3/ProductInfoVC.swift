82db0be22bf346d4b249c69c52cb0673647db13d
aleem javeed <aleemjaveed@hotmail.com>
2018-03-08 02:41:36 +0000

    basket Ui created

---
+++ FleetV3/Fleetv3/Fleetv3/ProductInfoVC.swift
@@ -0,0 +1,20 @@
+//
+//  ProductInfoVC.swift
+//  Fleetv3
+//
+//  Copyright © 2018 Aleem Javeed. All rights reserved.
+//
+
+import UIKit
+
+class ProductInfoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
