335014aa7c07443348cd18b702379eb6036a1879
aleem javeed <aleemjaveed@hotmail.com>
2018-03-08 04:16:33 +0000

    added stripe card payment and UI

---
+++ FleetV3/Fleetv3/Fleetv3/PayVC.swift
@@ -0,0 +1,21 @@
+//
+//  PayVC.swift
+//  Fleetv3
+//
+//  Copyright © 2018 Aleem Javeed. All rights reserved.
+//
+
+import UIKit
+import Stripe
+
+class PayVC: UIViewController {
+
+    @IBOutlet weak var cardTF: STPPaymentCardTextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ FleetV3/Fleetv3/Pods/Target Support Files/Pods-Fleetv3/Pods-Fleetv3-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Fleetv3 : NSObject
+@end
+@implementation PodsDummy_Pods_Fleetv3
+@end
